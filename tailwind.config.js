16f78f6814f257677b11796c2b2722a3b1928d66
chrisIyer <hellohotbox420@gmail.com>
2025-02-10 19:15:34 -0500

    the one issue i'm facing is that i can't run a babel plugin in react nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{js,tsx,ts,jsx}', "./<custom directory>/**/*.{js, jsx, ts,tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
