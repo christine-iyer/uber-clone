c573cdc616ffb69e562372c1b09abde4c72f36f1
chrisIyer <hellohotbox420@gmail.com>
2025-02-12 17:00:38 -0500

    it's worse.

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
+
+const Home =()=>{
+     return(
+          <SafeAreaView>
+               <Text>Hime</Text>
+          </SafeAreaView>
+     )
+}
+export default Home;
--- metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver = {
-  sourceExts: ["jsx", "js", "ts", "tsx"], // Ensure Metro processes the right files
-};
-
-module.exports = config;
